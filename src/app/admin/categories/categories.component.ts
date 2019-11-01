8f208702ed5736ef4d72d5814d94c19d852dafff
Isah Ohieku <isahohieku@gmail.com>
2019-11-01 19:19:45 +0100

    resolved getting categories and adding

--- src/app/admin/categories/categories.component.ts
+++ src/app/admin/categories/categories.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { CreateCategoryModalComponent } from 'src/app/util/create-category-modal/create-category-modal.component';
 import { DeleteItemModalComponent } from 'src/app/util/delete-item-modal/delete-item-modal.component';
+import { CrudService } from 'src/app/services/crud.service';
 
 @Component({
   selector: 'app-categories',
@@ -10,9 +11,23 @@ import { DeleteItemModalComponent } from 'src/app/util/delete-item-modal/delete-
 })
 export class CategoriesComponent implements OnInit {
 
-  constructor(private dialog: MatDialog) { }
+  constructor(
+    private dialog: MatDialog,
+    private crud: CrudService
+  ) { }
 
   ngOnInit() {
+    this.getCategories();
+  }
+
+  getCategories() {
+    const url = `categories`;
+
+    this.crud.getAllMethod(url)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(e => console.log(e));
   }
 
   openCreateModal() {
@@ -20,7 +35,7 @@ export class CategoriesComponent implements OnInit {
     dialogConfig.disableClose = false;
     dialogConfig.autoFocus = false;
     this.dialog.open(CreateCategoryModalComponent, dialogConfig)
-      .afterClosed().subscribe(_ => {});
+      .afterClosed().subscribe(_ => { });
   }
 
   deleteModal() {
@@ -28,7 +43,7 @@ export class CategoriesComponent implements OnInit {
     dialogConfig.disableClose = false;
     dialogConfig.autoFocus = false;
     this.dialog.open(DeleteItemModalComponent, dialogConfig)
-      .afterClosed().subscribe(_ => {});
+      .afterClosed().subscribe(_ => { });
   }
 
 }
